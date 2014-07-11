d839ad2acdae9eadd6092651f0c4cd06bf9ed6b2
Carlos Rosquillas <crosquillas@gmail.com>
2014-07-11 12:27:42 -0700

    Encrypt method scaffold

--- src/android/Safe.java
+++ src/android/Safe.java
@@ -1,4 +1,8 @@
-package com.bridge.safe;
+/**
+ * @author Carlos Antonio
+ * @version 0.1.0
+*/
+package com.bridge;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -10,23 +14,41 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class CDVsafe extends CordovaPlugin {
+public class Safe extends CordovaPlugin {
+
+  public static final String ENCRYPT_ACTION = "encrypt";
+  public static final String DECRYPT_ACTION = "decrypt";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
+      if (action.equals(ENCRYPT_ACTION)) {
+        String path = args.getString(0);
+        this.encryptFile(path, callbackContext);
+        return true;
+      }
+
+      if (action.equals(DECRYPT_ACTION)) {
+        String path = args.getString(0);
+        this.decryptFile(path, callbackContext);
+        return true;
+      }
+
+      return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    private void encryptFile(String path, CallbackContext callbackContext) {
+      if (path != null && path.length() > 0) {
+        callbackContext.success(path);
+      } else {
+        callbackContext.error(2);
+      }
+    }
+
+    private void decryptFile(String path, CallbackContext callbackContext) {
+      if (path != null && path.length() > 0) {
+        callbackContext.success(path);
+      } else {
+        callbackContext.error(2);
+      }
     }
 }
