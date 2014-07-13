e9f18a3b096e874758ecd54195839dba70cf711c
Carlos Rosquillas <crosquillas@gmail.com>
2014-07-13 12:35:47 -0700

    Remove duplicate version information
    (+1 squashed commit)
    Squashed commits:
    [06303e1] Remove duplicate version information

--- www/bridge.safe.js
+++ www/bridge.safe.js
@@ -1,10 +1,10 @@
 /**
- * @title Safe - cordova.plugins.bridge.safe
+ * bridge.safe.js
+ *
+ * @author Carlos Antonio
  * @overview Easy to use cryptographic operations for Cordova.
  * @copyright © 2014 cordova-bridge
  * @license GPLv2
- * @author Carlos Antonio
- * @version 0.1.0
 */
 
 var exec = require('cordova/exec');
