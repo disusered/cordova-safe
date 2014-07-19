a462dc998449b5e3ecee733d5aca34a9cf1fbb2b
Carlos Rosquillas <crosquillas@gmail.com>
2014-07-18 17:42:48 -0700

    Import required libs

--- src/android/Safe.java
+++ src/android/Safe.java
@@ -13,9 +13,11 @@ package com.bridge;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.apache.cordova.CordovaPlugin;
