c9ecc7e7d31fff9b5a0ad6c4e879e09ce4a2a871
Carlos Rosquillas <crosquillas@gmail.com>
2014-07-18 17:42:26 -0700

    Update license

--- src/android/Safe.java
+++ src/android/Safe.java
@@ -1,13 +1,12 @@
 /**
  * Safe.java
- *
+ * 
  * Copyright (C) 2014 Carlos Antonio
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
  */
 
 package com.bridge;
