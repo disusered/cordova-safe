46ce93aa4a6e4f37d1b354ef22f085550d0cba32
Carlos Rosquillas <crosquillas@gmail.com>
2014-07-19 18:07:03 -0700

    DRYer methods

--- src/android/Safe.java
+++ src/android/Safe.java
@@ -37,27 +37,39 @@ import com.facebook.crypto.keychain.SharedPrefsBackedKeyChain;
 import com.facebook.crypto.util.SystemNativeCryptoLibrary;
 
 /**
- * This class echoes a string called from JavaScript.
+ * This class encrypts and decrypts files using the Conceal encryption lib
  */
 public class Safe extends CordovaPlugin {
 
   public static final String ENCRYPT_ACTION = "encrypt";
   public static final String DECRYPT_ACTION = "decrypt";
 
+  private Context CONTEXT;
+  private Crypto CRYPTO;
+  private Entity ENTITY;
+
+  private OutputStream OUTPUT_STREAM;
+  private InputStream INPUT_STREAM;
+
+  private String FILE_NAME;
+  private File SOURCE_FILE;
+  private File TEMP_FILE;
+
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
       throws JSONException {
-    if (action.equals(ENCRYPT_ACTION)) {
+    if (action.equals(ENCRYPT_ACTION) || action.equals(DECRYPT_ACTION)) {
       String path = args.getString(0);
       String pass = args.getString(1);
-      this.encryptFile(path, pass, callbackContext);
-      return true;
-    }
 
-    if (action.equals(DECRYPT_ACTION)) {
-      String path = args.getString(0);
-      String pass = args.getString(1);
-      this.decryptFile(path, pass, callbackContext);
+      if (action.equals(ENCRYPT_ACTION)) {
+        this.encryptFile(path, pass, callbackContext);
+      }
+
+      if (action.equals(DECRYPT_ACTION)) {
+        this.decryptFile(path, pass, callbackContext);
+      }
+
       return true;
     }
 
@@ -65,97 +77,88 @@ public class Safe extends CordovaPlugin {
   }
 
   private void encryptFile(String path, String password, CallbackContext callbackContext) {
-    if (path != null && path.length() > 0 && password != null && password.length() > 0) {
-      // get cordova context
-      Context context = cordova.getActivity().getApplicationContext();
+    // init crypto variables
+    this.initCrypto(path, password, callbackContext);
 
-      // create entity based on pass
-      Entity entity = new Entity(password);
+    // create output stream which encrypts the data as
+    // it is written to it and writes out to the file
+    try {
+      OutputStream encryptedOutputStream = CRYPTO.getCipherOutputStream(OUTPUT_STREAM, ENTITY);
 
-      // get uri of source file based on path
-      Uri uri = Uri.parse(path);
+      // create new byte object with source file length
+      byte[] data = new byte[(int) SOURCE_FILE.length()];
 
-      // get source file based on uri
-      File file = new File(uri.getPath());
-
-      // get name of source file based on uri
-      String fileName = uri.getLastPathSegment();
-
-      // create temp file for writing encrypted output stream
-      File tmpFile = null;
-      try {
-        tmpFile = File.createTempFile(fileName, null, context.getCacheDir());
-      } catch (IOException e) {
-        callbackContext.error(0);
-        e.printStackTrace();
+      // read contents of source file byte by byte
+      int buffer = 0;
+      while ((buffer = INPUT_STREAM.read(data)) > 0) {
+        // write contents to encrypted output stream
+        encryptedOutputStream.write(data, 0, buffer);
+        encryptedOutputStream.flush();
       }
 
-      // create crypto object with defaults
-      Crypto crypto =
-          new Crypto(new SharedPrefsBackedKeyChain(context), new SystemNativeCryptoLibrary());
+      // close encrypted output stream
+      encryptedOutputStream.close();
+      INPUT_STREAM.close();
 
-      // check for whether crypto is available
-      if (!crypto.isAvailable()) {
+      // delete original file
+      boolean deleted = SOURCE_FILE.delete();
+      if (deleted) {
+        callbackContext.success(TEMP_FILE.getPath());
+      } else {
         callbackContext.error(1);
-        return;
       }
-
-      // create output stream to temp file
-      OutputStream outputStream = null;
-      try {
-        outputStream = new BufferedOutputStream(new FileOutputStream(tmpFile));
-      } catch (FileNotFoundException e) {
-        callbackContext.error(4);
-        e.printStackTrace();
-      }
-
-      // create output stream which encrypts the data as
-      // it is written to it and writes out to the file
-      try {
-        OutputStream encryptedOutputStream = crypto.getCipherOutputStream(outputStream, entity);
-
-        // create input stream from source file
-        InputStream inputStream = new FileInputStream(file);
-
-        // create new byte object with source file length
-        byte[] data = new byte[(int) file.length()];
-
-        // read contents of source file byte by byte
-        int buffer = 0;
-        while ((buffer = inputStream.read(data)) > 0) {
-          // write contents to encrypted output stream
-          encryptedOutputStream.write(data, 0, buffer);
-          encryptedOutputStream.flush();
-        }
-
-        // close encrypted output stream
-        encryptedOutputStream.close();
-        inputStream.close();
-
-        // delete original file
-        boolean deleted = file.delete();
-        if (deleted) {
-          callbackContext.success(tmpFile.getPath());
-        } else {
-          callbackContext.error(1);
-        }
-      } catch (IOException e) {
-        callbackContext.error(e.getMessage());
-      } catch (CryptoInitializationException e) {
-        callbackContext.error(e.getMessage());
-      } catch (KeyChainException e) {
-        callbackContext.error(e.getMessage());
-      } catch (Exception e) {
-        callbackContext.error(e.getMessage());
-      }
-    } else {
-      callbackContext.error(2);
+    } catch (IOException e) {
+      callbackContext.error(e.getMessage());
+    } catch (CryptoInitializationException e) {
+      callbackContext.error(e.getMessage());
+    } catch (KeyChainException e) {
+      callbackContext.error(e.getMessage());
+    } catch (Exception e) {
+      callbackContext.error(e.getMessage());
     }
   }
 
   private void decryptFile(String path, String password, CallbackContext callbackContext) {
     if (path != null && path.length() > 0) {
       callbackContext.success(path);
+    } else {
+      callbackContext.error(2);
+    }
+  }
+
+  private void initCrypto(String path, String password, CallbackContext callbackContext) {
+    if (path != null && path.length() > 0 && password != null && password.length() > 0) {
+      Uri uri = Uri.parse(path);
+
+      CONTEXT = cordova.getActivity().getApplicationContext();
+      ENTITY = new Entity(password);
+
+      FILE_NAME = uri.getLastPathSegment();
+      SOURCE_FILE = new File(uri.getPath());
+
+      // initialize crypto object
+      CRYPTO = new Crypto(new SharedPrefsBackedKeyChain(CONTEXT), new SystemNativeCryptoLibrary());
+
+      // check for whether crypto is available
+      if (!CRYPTO.isAvailable()) {
+        callbackContext.error(1);
+        return;
+      }
+
+      try {
+        // initialize temp file
+        TEMP_FILE = File.createTempFile(FILE_NAME, null, CONTEXT.getCacheDir());
+        // initialize output stream for temp file
+        OUTPUT_STREAM = new BufferedOutputStream(new FileOutputStream(TEMP_FILE));
+        // create input stream from source file
+        INPUT_STREAM = new FileInputStream(SOURCE_FILE);
+      } catch (FileNotFoundException e) {
+        callbackContext.error(e.getMessage());
+        e.printStackTrace();
+      } catch (IOException e) {
+        callbackContext.error(e.getMessage());
+        e.printStackTrace();
+      }
     } else {
       callbackContext.error(2);
     }
