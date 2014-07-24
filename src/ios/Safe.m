a83f3be3838abe6bb1c482b6706090ed225383f0
Carlos Rosquillas <crosquillas@gmail.com>
2014-07-23 18:49:36 -0700

    Add encrypt and decrypt skeleton methods

--- src/ios/Safe.m
+++ src/ios/Safe.m
@@ -1,18 +1,43 @@
-/********* CDVsafe.m Cordova Plugin Implementation *******/
+/**
+ * Safe.m
+ *
+ * Copyright (C) 2014 Carlos Antonio
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ */
 
-#import <Cordova/CDV.h>
+#import "Safe.h"
 
-@interface CDVsafe : CDVPlugin {
-  // Member variables go here.
+@implementation Safe
+
+/**
+ *  encrypt
+ *
+ *  @param command An array of arguments passed from javascript
+ */
+- (void)encrypt:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
-@end
-
-@implementation CDVsafe
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
-{
+/**
+ *  decrypt
+ *
+ *  @param command An array of arguments passed from javascript
+ */
+- (void)decrypt:(CDVInvokedUrlCommand *)command {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
 
